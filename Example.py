f06f3e13adc3b9e0d1027c103309f7e66adc4ac5
Mohammad <moh@mo-pc.localdomain>
2019-06-20 00:42:17 +0430

    gitignore

--- Example.py
+++ Example.py
@@ -1,8 +1,7 @@
-
 from Sierpinski import SierpinskiPlot
 sier = SierpinskiPlot(5,col='purple')
 sier
 #sier.savefig("Sierpinski.pdf", bbox_inches='tight')
 ###Example2:
-b=SierpinskiPlot(4,tri0 = [[-2, 0], [2, 0], [-1, 3]])
+b=SierpinskiPlot(6,tri0 = [[-2, 0], [2, 0], [-1, 3]])
 b
